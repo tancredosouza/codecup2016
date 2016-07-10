62ef9a75b39fc644dda4b45f1fbd4a30fff27727
Erik Zambom <erikzambom@hotmail.com>
2016-07-10 09:15:11 -0300

    parte 4

--- codecup/views/aulas/_form.php
+++ codecup/views/aulas/_form.php
@@ -2,21 +2,19 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use app\models\user;
-use wbraganca\tagsinput\TagsinputWidget;
-
+use \kartik\select2\Select2;
+use kartik\switchinput\SwitchInput;
 /* @var $this yii\web\View */
 /* @var $model app\models\Aulas */
 /* @var $form yii\widgets\ActiveForm */
-$user_id = Yii::$app->user->id;
 ?>
 
 <div class="aulas-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_user')->hiddenInput(
-            ['value' => $user_id]
+        <?= $form->field($model, 'id_user')->hiddenInput(
+            ['value' => Yii::$app->user->identity->id]
         ) ->label(false)
     ?>
 
@@ -24,21 +22,23 @@ $user_id = Yii::$app->user->id;
 
     <?= $form->field($model, 'descricao')->textarea(['rows' => 6])->hint('Fale mais sobre a aula.') ?>
 
-    <?= $form->field($model, 'tags')->widget(TagsinputWidget::classname(), [
-            'clientOptions' => [
-                'trimValue' => true,
-                'allowDuplicates' => false
-            ]
+     <?= $form->field($model, 'tags')->widget(Select2::classname(), [
+            'maintainOrder' => true,
+            'options' => ['placeholder' => 'Tags ...', 'multiple' => true],
+            'pluginOptions' => [
+                'tags' => true,
+                'maximumInputLength' => 30
+            ],
         ])
     ?>
 
-    <?= $form->field($model, 'disponibilidade')->hiddenInput(
-            ['value' => true]
-        )->label(false) // tem ';' ??
-     ?>
+    <?= $form->field($model, 'disponibilidade')->widget(SwitchInput::classname(), [
+        'type' => SwitchInput::CHECKBOX
+    ]) ?>
+
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Criar' : 'Editar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Cadastrar' : 'Editar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
