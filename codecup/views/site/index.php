5b4d5e78268ee06e83c64f77e53dbb322f2bf22b
Erik Zambom <erikzambom@hotmail.com>
2016-07-10 01:27:40 -0300

    Changes Home

--- codecup/views/site/index.php
+++ codecup/views/site/index.php
@@ -1,53 +1,40 @@
-<?php
-
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+<header id="intro">
+    <div class="container">
+        <div class="table">
+            <div class="header-text">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                      <h1 class="typed" style= "color: #0066cc">O que você quer aprender hoje?</h1>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
+</header>
+
+<div class="container">
+    <section id="tutorial" class="section section-padded">
+        <div class="container">
+            <div class="row text-center title">
+                <h2>Como funciona?</h2>
+            </div>
+        </div>
+        <div class="cut cut-bottom"></div>
+    </section>
+    <section id="aulas" class="section gray-bg">
+        <div class="container">
+            <div class="row title text-center">
+                <h2 class="margin-top">Aulas</h2>
+            </div>
+            
+        </div>
+    </section>
+    <section id="sobre" class="section">
+        <div class="container">
+            <div class="row title text-center">
+                <h2 class="margin-top">Sobre</h2>
+                <h4 class="light white">legenda: sobre</h4>
+            </div>
+        </div>
+    </section>
 </div>
